3c3e898be72bdcfb3714e3ad46c3f00e59a49f63
James Butcher <james@machine.lan>
2024-07-13 18:08:58 +0100

    Implement simulate function to simulate L-system progression

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Data.HashMap.Strict (HashMap, fromList, toList)
+import Data.HashMap.Strict (HashMap, fromList, toList, (!))
 
 data Rule = Rule { weight :: Float
                  , output :: [Int]
@@ -13,15 +13,25 @@ rules = fromList [ (1, [Rule { weight = 1
                  , (2, [Rule { weight = 1, output = [1]}])
                  ]
 
+getRuleOutput :: Int -> [Int]
+getRuleOutput k = output . head $ (!) rules k
+
+simulate :: [Int] -> Int -> [Int]
+simulate xs gens | gens >= 1 = simulate next $ gens - 1
+                 | otherwise = xs
+  where next = concatMap getRuleOutput xs
+  
+-- Check the sum of all weights per rule adds up to 1
 checkRules :: Bool
 checkRules = checkRules' $ toList rules
-
 checkRules' :: [(Int, [Rule])] -> Bool
 checkRules' [] = True
 checkRules' ((_, x):xs) = (weightSum x == 1) && checkRules' xs
 
+-- Calculate the sum of the weights
 weightSum :: [Rule] -> Float
 weightSum = foldr (\rule acc -> weight rule + acc) 0
 
 main :: IO ()
-main = if checkRules then putStrLn "Hello, Haskell!" else putStrLn "Weights do not add up"
+main | checkRules = print $ simulate [1] 2 
+     | otherwise = putStrLn "Weight sum is not 1 for each rule"
